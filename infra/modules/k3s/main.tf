c534eefd0c17cf5de1a5784ee09b50238e267772
Arunprabu Siva <arunprabusiva@outlook.com>
2025-07-29 00:43:55 +0530

    Update main.tf

--- infra/modules/k3s/main.tf
+++ infra/modules/k3s/main.tf
@@ -228,8 +228,8 @@ fi
 
 # Install K3s with write permissions and bind to all interfaces
 PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
-PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --bind-address=0.0.0.0 --advertise-address=$PUBLIC_IP --node-external-ip=$PUBLIC_IP --tls-san=$PUBLIC_IP --tls-san=$PRIVATE_IP
+echo "Installing K3s with public IP: $PUBLIC_IP"
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --bind-address=0.0.0.0 --advertise-address=$PUBLIC_IP
 
 # Setup Docker
 systemctl enable docker
