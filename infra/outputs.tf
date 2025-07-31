2b72c45d11a1979ecbec2213658ab676181fe35a
Arunprabu Siva <arunprabusiva@outlook.com>
2025-07-31 18:37:45 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -53,16 +53,16 @@ output "cluster_ips" {
 
 
 
-# RDS Outputs
-output "db_instance_endpoint" {
-  description = "Connection endpoint for the RDS database"
-  value       = var.database_config != null ? module.rds[0].db_instance_endpoint : null
-}
-
-output "db_instance_name" {
-  description = "Name of the RDS instance"
-  value       = var.database_config != null ? module.rds[0].db_instance_name : null
-}
+# RDS Outputs - commented out since RDS module is disabled
+# output "db_instance_endpoint" {
+#   description = "Connection endpoint for the RDS database"
+#   value       = var.database_config != null ? module.rds[0].db_instance_endpoint : null
+# }
+#
+# output "db_instance_name" {
+#   description = "Name of the RDS instance"
+#   value       = var.database_config != null ? module.rds[0].db_instance_name : null
+# }
 
 # Deployment Outputs
 # output "kubernetes_namespace" {
